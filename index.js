985900f09865bcf178052c37d571c7461a1613b5
Mahdi Hasan Mahi <mh7266391@gmail.com>
2024-08-14 22:05:24 +0600

    mongodb added

--- index.js
+++ index.js
@@ -18,6 +18,45 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrdgddr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db(`${process.env.DB_USER}`);
+    const productCollection = db.collection("products");
+
+    // get product amount
+    app.get("/count", async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ amount: count });
+    });
+
+    // get all products
+    app.get("/products", async (req, res) => {
+      const products = await productCollection.find().toArray();
+      res.send(products);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello from Server..");
 });
